56dc5be52f63930460c09845d6d0c01dce44d2a7
Wallace dos Anjos <wallace.anjos@al.infnet.edu.br>
2020-12-06 16:54:43 -0300

    Add project files.

---
+++ TaskList.Domain/Entities/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskList.Domain.Entities
+{
+    public class TaskItem
+    {
+        public Guid Id { get; set; }
+        public string TaskTitle { get; set; }
+        public string TaskDetails { get; set; }
+    }
+}
---
+++ TaskList.Infrastructure/TaskListDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskList.Domain.Entities;
+
+namespace TaskList.Infrastructure
+{
+    public class TaskListDbContext : DbContext
+    {
+        public DbSet<TaskItem> TaskItems { get; set; }
+
+        public TaskListDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TaskListDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+
+    
+}
